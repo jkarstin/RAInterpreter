6e2835cc165d222d19f2187333adcce0495dbcdd
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2020-04-28 14:36:02 -0700

    Initial commit

---
+++ desktop-rai/src/neil1648/cs360/ui/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package neil1648.cs360.ui.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import neil1648.cs360.ui.RAInterpreter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RAInterpreter(), config);
+	}
+}
